f23c4a24e9b5ab014281de720901e3131a3f2e11
Vaishnavi Pawar <vpvaishnavipawar@gmail.com>
2025-02-22 21:24:42 -0500

    Updated DELETE API

--- index.js
+++ index.js
@@ -90,10 +90,20 @@ app.put('/users/:id', async (req, res) => {
 
 
 // Delete a User
-app.delete('/users/:id', (req, res) => {
-    users = users.filter(u => u.id !== parseInt(req.params.id));
-    res.json({ message: 'User deleted' });
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
 
 // Server Listening
 app.listen(PORT, () => {
