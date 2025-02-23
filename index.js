e5baef3517d86470241cd019b8105348702c72c5
Vaishnavi Pawar <vpvaishnavipawar@gmail.com>
2025-02-22 22:17:10 -0500

    Updated Index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+
 const mongoose = require('mongoose');
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
